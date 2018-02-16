13159fa32520d938bae25feafc3978a434cfcefc
Abdullah Yahya <abdullah747@gmail.com>
2018-02-15 17:50:39 -0800

    First pass at CSO Interchange home page

--- site/pages/index.js
+++ site/pages/index.js
@@ -1,1 +1,17 @@
-console.log("index.js");
+// init customers carousel
+var swiper = new Swiper('.swiper-container', { // eslint-disable-line
+	direction: 'horizontal',
+	loop: false,
+	slidesPerView: 'auto',
+	scrollbar: false,
+	nextButton: '.q-carousel-button--next',
+	prevButton: '.q-carousel-button--prev',
+	buttonDisabledClass: 'q-carousel-button--disabled',
+
+	spaceBetween: 30
+});
+
+// swiper init workaround
+setTimeout(function () {
+	swiper.update(true);
+}, 100);
