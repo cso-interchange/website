3ea32df264e4e226e0836b2e1a81a3bafe5231aa
Keith Shaw <github@keithws.net>
2018-05-16 20:32:03 -0700

    add preconnent for pingdom; updated swiper to latest

--- site/pages/index.js
+++ site/pages/index.js
@@ -8,18 +8,16 @@ var swiper = new Swiper('.swiper-container', { // eslint-disable-line
 	direction: 'horizontal',
 	loop: false,
 	slidesPerView: 'auto',
+	watchSlidesProgress: true,
+	watchSlidesVisibility: true,
 	scrollbar: false,
-	nextButton: '.q-carousel-button--next',
-	prevButton: '.q-carousel-button--prev',
-	buttonDisabledClass: 'q-carousel-button--disabled',
-
+	navigation: {
+		nextEl: '.q-carousel-button--next',
+		prevEl: '.q-carousel-button--prev',
+		disabledClass: 'q-carousel-button--disabled'
+	},
 	spaceBetween: 30
 });
-
-// swiper init workaround
-setTimeout(function () {
-	swiper.update(true);
-}, 100);
 
 // expand / collapse agenda rows
 (function ($) {
