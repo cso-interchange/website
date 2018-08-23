dedfbe9e5937ef92dd58cda84b474b704d33947e
Gautam Nandane <gnandane@qualys.com>
2018-08-23 14:59:48 +0530

    completed the agenda page, created registration and registration confirmation pages.

---
+++ site/pages/agenda/index.js
@@ -0,0 +1,40 @@
+// expand / collapse agenda rows
+(function ($) {
+	"use strict";
+
+	$(".title-speaker-container").on("click", function(event){
+		if ($(event.currentTarget).hasClass("expanded")) {
+			$(event.currentTarget).removeClass("expanded").addClass("collapsed");
+			$(event.currentTarget).parent(".description").find(".abstract").addClass("hidden");
+		} else if ($(event.currentTarget).hasClass("collapsed")) {
+			$(event.currentTarget).removeClass("collapsed").addClass("expanded");
+			$(event.currentTarget).parent(".description").find(".abstract").removeClass("hidden");
+		}
+	});
+
+}(jQuery));
+
+/**
+ * expand / collapse long passages of text
+ */
+(function ($) {
+	"use strict";
+
+	$(".bio-wrapper.expandable").on("click", function(event){
+		if ($(event.currentTarget).hasClass("expanded")) {
+			$(event.currentTarget).removeClass("expanded");
+		} else {
+			$(event.currentTarget).addClass("expanded");
+		}
+	});
+
+	// when click bio link, expand person's bio
+	$(".bio-link").on("click", function (event) {
+		var speaker = $(event.currentTarget).attr("data-speaker");
+		var $bioWrapper = $("#"+speaker + " .bio-wrapper");
+		if ($bioWrapper.hasClass("expanded") === false) {
+			$bioWrapper.click();
+		}
+	});
+
+}(jQuery));
